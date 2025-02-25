2834e5deaff3cc8a9919d8522267b7d1265e1c5d
erikamc99 <erikamc99@gmail.com>
2025-02-25 15:17:17 +0100

    structure RecordButtons created

---
+++ src/components/piano/config/record_buttons/RecordButtons.jsx
@@ -0,0 +1,13 @@
+
+
+export const RecordButtons = () => {
+
+
+    return (
+        <div className="record-buttons">
+            <button className="rec-button"></button>
+            <button className="record-button"><img src="/public/img/icons/play_icon.png" alt="play icon" /></button>
+            <button className="record-button"><img src="/public/img/icons/pause_icon.png" alt="pause icon" /></button>
+        </div>
+    )
+}
