bd8ba28359c801bcaceb582d18ed78d5d9621a5f
Rosa <angelica-2025@outlook.es>
2025-02-27 16:35:42 +0100

    Create estructure HomeView without style

---
+++ src/components/homeView/HomeView.jsx
@@ -0,0 +1,19 @@
+import "./HomeView.css";
+
+export const HomeView = () => {
+
+
+    return ( 
+        <div className="container">
+
+            <div className="container-superior">
+                <Menu />
+                <Modales />
+            </div>
+             
+             <Piano />
+
+        </div>
+
+    )
+}
