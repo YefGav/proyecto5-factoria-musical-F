cbddefcbcbe12edbfc26a04ab5f8d2654147bf6c
erikamc99 <erikamc99@gmail.com>
2025-02-27 23:09:41 +0100

    songs and song styles fixed but non-finished

--- src/components/modals/songs/song/Song.jsx
+++ src/components/modals/songs/song/Song.jsx
@@ -6,8 +6,8 @@ export const Song = ({ imgSrc, title, song }) => {
   const { openModal } = useModal();
 
     return (
-      <div className="score" onClick={() => openModal("score", song)}>
-        <img src={imgSrc} alt={title} />
+      <div className="song" onClick={() => openModal("score", song)}>
+        <img className="song-img" src={imgSrc} alt={title} />
         <p>{title}</p>
       </div>
     );
