0eccc7c77d3e2185bd5dd62d5839d149a13822ae
MabelRincon <mabel.rincon@outlook.com>
2025-02-28 19:23:58 +0100

    Add favicon fix broken logo and fix document format

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,32 +1,21 @@
 import './Menu.css'
-import { MenuButton } from './menu-button/MenuButton';
-
+import { MenuButton } from './menu-button/MenuButton'
 
 export const Menu = () => {
-  
-  
-
-
   return (
     <div className="menu-container">
       <div className="init-app">
         <img
-          src="/public/img/icons/logoFM-black.png"
+          src="/public/img/icons/logoFactoriaMusical-black.png"
           alt="Factoria Musical Play with us!"
           className="img-logo"
         />
 
         <div className="buttons">
-          
-           <MenuButton action="openLevel"  text = "JUGAR" />
-           <MenuButton action="openRecordings" text = "GRABACIONES" />
-        
+          <MenuButton action="openLevel" text="JUGAR" />
+          <MenuButton action="openRecordings" text="GRABACIONES" />
         </div>
       </div>
-
- 
-      
     </div>
   )
 }
-
